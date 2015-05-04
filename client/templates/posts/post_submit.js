fb77f8c1092328b7e944874da4fbafe8cca390f4
leiyinghao <leiyinghao.bj@gmail.com>
2015-05-04 10:39:11 +0800

    }

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,6 +7,13 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    Meteor.call('postInsert', post, function(error, result) {
+      // 显示错误信息并退出
+      if (error)
+        return alert(error.reason);
+      Router.go('postPage', {_id: result._id});  
+    });
+
     post._id = Posts.insert(post);
     Router.go('postPage', post);
   }
