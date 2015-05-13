7d5460f30aed8e0464050f7be569fe2e2fc0cc23
leiyinghao <leiyinghao.bj@gmail.com>
2015-05-13 16:09:57 +0800

    12-6 使用单一帖子订阅来确保我们总会看到正确的帖子

--- server/publications.js
+++ server/publications.js
@@ -6,10 +6,10 @@ Meteor.publish('posts',function(options){
 	return Posts.find({},options)
 })
 
-Meteor.publish('singlePost',function(id){
-	check(id,String)
-	return Posts.find(id);
-})
+Meteor.publish('singlePost', function(id) {
+  check(id, String)
+  return Posts.find(id);
+});
 
 Meteor.publish('comments', function(postId) {
   check(postId, String);
